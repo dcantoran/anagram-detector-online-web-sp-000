c3ec9822d115112d407377d7e373882d3c7b789d
David Cantoran <davidcantoran36@gmail.com>
2019-12-17 05:00:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   def match(words_arr)
     sorted_word = @word.split("").sort 
     sorted_words_arr = words_arr.collect{|i| i.split("").sort}
-    
+    sorted_words_arr.select{|set| set == sorted_word}.flatten
   end 
   
   
